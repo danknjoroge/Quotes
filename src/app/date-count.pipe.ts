a0dcf1e0dec3e87ef9b6f3cf3bf8838838a38d56
danknjoroge <dank8505@gmail.com>
2022-02-10 11:47:08 +0300

    added custom pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,19 +5,32 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: any,): number {
-    let today:Date = new Date();
-    let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-    var dateDifference = Math.abs(value - todayWithNoTime)
-    const secondsInDay = 86400; 
-    var dateDifferenceSeconds = dateDifference*0.001;
-    var dateCounter = dateDifferenceSeconds/secondsInDay;
-
-    if (dateCounter >= 1 || dateCounter <=1 && value > todayWithNoTime){
-      return dateCounter;
-    }else{
-      return 0;
+  transform(value: any, args?: any): any {
+    if (value) {
+      const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+      if (seconds < 29)
+        return 'Just now';
+      const intervals:any = {
+        'year': 31536000,
+        'month': 2592000,
+        'week': 604800,
+        'day': 86400,
+        'hour': 3600,
+        'minute': 60,
+        'second': 1
+      };
+      let dateCounter;
+      for (const i in intervals) {
+        dateCounter = Math.floor(seconds / intervals[i]);
+        if (dateCounter > 0)
+          if (dateCounter === 1) {
+            return dateCounter + ' ' + i + ' ago'; 
+          } else {
+            return dateCounter + ' ' + i + 's ago'; 
+          }
+      }
     }
+    return value;
   }
 
 }
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,6 +1,6 @@
 export class Quote {
     // [x: string]: Date;
-    showAuthor: boolean
+    showAuthor: boolean;
     constructor(public id: number, public quote: string, public author: string, public submitter: string, public postDate: Date, public upvote:number, public downvote:number) {
         this.showAuthor=false;
     }
